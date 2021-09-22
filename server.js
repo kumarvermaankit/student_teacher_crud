33942c725940d779d907f30b26a68bcf61256ae8
kumar <kumarvermaankit9555@gmail.com>
2021-09-22 11:17:56 +0530

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const teacher = require("./controllers/teacher_operations")
-
+const student = require("./controllers/student_operation")
 require('dotenv').config();
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,7 +19,7 @@ catch (err) {
 }
 
 app.use("/teacher", teacher)
-
+app.use("/student", student)
 
 app.listen(port, () => {
     console.log("Server is running");
